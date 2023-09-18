ce93139b8abb1e8a1dbc8ee3462df3731d57afff
BJ-Series <143959532+BJ-Series@users.noreply.github.com>
2023-09-18 15:22:04 -0300

    Update MyTheory.js

--- MyTheory.js
+++ MyTheory.js
@@ -1,1 +1,15 @@
-lol import lol
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "CanadaTheory";
+var name = "Canada";
+var description = "The draft bulids on next release is not draft.";
+var authors = "BJ-Series";
+var version = "1.0.0";
+
+var currency;
+
+currency = theory.createCurrency();
